ada22322046180a397dee395b310f59d14201b18
cam iverson <camiver@gmail.com>
2016-05-02 09:46:35 -0500

    three tests made to verify data in hash

--- test/movie_test.rb
+++ test/movie_test.rb
@@ -8,4 +8,16 @@ describe Movie do
   it "should return a hash" do
     Movie.new.to_hash.class.should == Hash
   end
+
+  it "should have released in 1986" do
+    @top_gun_year = Movie.new.to_hash
+    @top_gun_year["Year"].should == "1986"
+  end
+
+  it "should have a rating of PG" do
+    @top_gun_rating = Movie.new.to_hash
+    @top_gun_rating["Rated"].should == "PG"
+  end
+
+
 end
